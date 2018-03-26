fe3226d6c131ac16705b7f34d7e3e4cf01f57494
Gergely Nagy <gergelynagy99@gmail.com>
2018-03-26 21:16:53 +0200

    Message processing replaced with regex.

--- src/main/java/util/MessageProcessor.java
+++ src/main/java/util/MessageProcessor.java
@@ -1,12 +1,17 @@
 package main.java.util;
 
+import java.util.regex.Pattern;
+
 import main.java.enums.Operation;
 import main.java.model.BaseMessage;
 import main.java.model.MessageTrade;
 import main.java.model.MessageModifier;
 
 public class MessageProcessor {
-	static private String NOT_NUMBERS = "\\D+";
+	static final private String NOT_NUMBERS = "\\D+";
+	static final private String MESSAGE_PATTERN_1 = "(?i)(\\S+)\\s*at\\s*(\\S+)";
+	static final private String MESSAGE_PATTERN_2 = "(?i)^(\\d). sales of (\\S+)\\s*at\\s*(\\S+) each";
+	static final private String MESSAGE_PATTERN_3 = "(?i)^(add|subtract|multiply).((?:\\S+\\s+){1}\\S+)";
 	
 	/**
 	   * This method to process messages
@@ -17,39 +22,46 @@ public class MessageProcessor {
 	   * @param msg  This is the message to process
 	   * @return BaseMessage This returns a message.
 	   */
-	public static BaseMessage process(String msg) throws Exception{
+	public static BaseMessage process(final String msg) throws Exception{
 		BaseMessage trade = null;
-		//Split string to words
-		String[] parts = msg.split(" ");
-		
-		if(parts.length == 3){
-			if(parts[1].equals("at")){
-				//template: {{item}} at {{price}}
-				
-				//Extract numbers from word
-				String nums = parts[2].replaceAll(NOT_NUMBERS,"");
-				trade = new MessageTrade(parts[0], 1, Integer.parseInt(nums));
-			} else {
-				//template: {{operation}} {{price}} {{item}}s
-				
-				String operation = parts[0].toUpperCase();
-				//Extract numbers from word
-				String nums = parts[1].replaceAll(NOT_NUMBERS,"");
-				//Remove last character
-				String product = parts[2].substring(0, parts[2].length() - 1);
-				trade = new MessageModifier(product, Operation.valueOf(operation), Integer.parseInt(nums));
-			}
-		} else if(parts.length == 7) {
-			//template: {{quantity}} sales of {{item}}s at {{price}} each 
-			
-			//Extract numbers from word
-			String nums = parts[5].replaceAll(NOT_NUMBERS,"");
-			//Remove last character
-			String product = parts[3].substring(0, parts[3].length() - 1);
-			trade = new MessageTrade(product, Integer.parseInt(parts[0]), Integer.parseInt(nums));
+		if(Pattern.matches(MESSAGE_PATTERN_2, msg)){
+			trade = parseMessage2(msg);
+		} else if(Pattern.matches(MESSAGE_PATTERN_1, msg)){
+			trade = parseMessage1(msg);
+		} else if(Pattern.matches(MESSAGE_PATTERN_3, msg)){
+			trade = parseMessage3(msg);
 		} else {
 			throw new Exception();
 		}
+		return trade;
+	}
+	
+	private static BaseMessage parseMessage1(final String msg){
+		String[] parts = msg.split(" ");
+		//Extract numbers from word
+		String nums = parts[2].replaceAll(NOT_NUMBERS,"");
+		BaseMessage trade = new MessageTrade(parts[0], 1, Integer.parseInt(nums));
+		return trade;
+	}
+	
+	private static BaseMessage parseMessage2(final String msg){
+		String[] parts = msg.split(" ");
+		//Extract numbers from word
+		String nums = parts[5].replaceAll(NOT_NUMBERS,"");
+		//Remove last character
+		String product = parts[3].substring(0, parts[3].length() - 1);
+		BaseMessage trade = new MessageTrade(product, Integer.parseInt(parts[0]), Integer.parseInt(nums));
+		return trade;
+	}
+	
+	private static BaseMessage parseMessage3(final String msg){
+		String[] parts = msg.split(" ");
+		String operation = parts[0].toUpperCase();
+		//Extract numbers from word
+		String nums = parts[1].replaceAll(NOT_NUMBERS,"");
+		//Remove last character
+		String product = parts[2].substring(0, parts[2].length() - 1);
+		BaseMessage trade = new MessageModifier(product, Operation.valueOf(operation), Integer.parseInt(nums));
 		return trade;
 	}
 }
